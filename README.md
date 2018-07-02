7cb1b59eb72503b08afaa22ad909822f5987fa41
Ajay-KG <ajay@plivo.com>
2018-07-02 12:49:37 +0530

    added mux routing, middleware etc

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # gowiki
 gowiki
+
+
+References:
+
+    1. https://tour.golang.org/list
+    2. https://gowebexamples.com
+    3. https://golang.org/doc/
--- wiki.go
+++ wiki.go
@@ -7,6 +7,10 @@ import (
 	"log"
 	"regexp"
 	"errors"
+	"github.com/gorilla/mux"
+	"fmt"
+	"time"
+	"encoding/json"
 )
 
 var templates = template.Must(template.ParseFiles("edit.html", "view.html"))
@@ -36,10 +40,14 @@ func loadPage(title string) (*Page, error) {
 //}
 
 func viewHandler(w http.ResponseWriter, r *http.Request) {
-	title, err := getTitle(w, r)
-	if err != nil {
-		return
-	}
+	//title, err := getTitle(w, r)
+	//if err != nil {
+	//	return
+	//}
+
+	vars := mux.Vars(r)
+	title := vars["title"]
+	log.Printf("Inside view function vars: %s, title: %s", vars, title)
 	p, err := loadPage(title)
 	if err != nil {
 		http.Redirect(w, r, "/edit/"+title, http.StatusFound)
@@ -49,13 +57,15 @@ func viewHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func saveHandler(w http.ResponseWriter, r *http.Request) {
-	title, err := getTitle(w, r)
-	if err != nil {
-		return
-	}
+	//title, err := getTitle(w, r)
+	//if err != nil {
+	//	return
+	//}
+	vars := mux.Vars(r)
+	title := vars["title"]
 	body := r.FormValue("body")
 	p := &Page{Title: title, Body: []byte(body)}
-	err = p.save()
+	err := p.save()
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
@@ -64,10 +74,12 @@ func saveHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func editHandler(w http.ResponseWriter, r *http.Request) {
-	title, err := getTitle(w, r)
-	if err != nil {
-		return
-	}
+	//title, err := getTitle(w, r)
+	//if err != nil {
+	//	return
+	//}
+	vars := mux.Vars(r)
+	title := vars["title"]
 	p, err := loadPage(title)
 	if err != nil {
 		p = &Page{Title: title}
@@ -82,6 +94,20 @@ func renderTemplate(w http.ResponseWriter, tmpl string, p *Page) {
 	}
 }
 
+//middleware
+func logging(f http.HandlerFunc) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		log.Println(r.URL.Path)
+		f(w, r)
+	}
+}
+
+func foo(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "foo")
+}
+func bar(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "bar")
+}
 func getTitle(w http.ResponseWriter, r *http.Request) (string, error) {
 	m := validPath.FindStringSubmatch(r.URL.Path)
 	if m == nil {
@@ -91,6 +117,84 @@ func getTitle(w http.ResponseWriter, r *http.Request) (string, error) {
 	return m[2], nil // The title is the second subexpression.
 }
 
+type Middleware func(http.HandlerFunc) http.HandlerFunc
+
+// Logging logs all requests with its path and the time it took to process
+func Logging() Middleware {
+
+	// Create a new Middleware
+	return func(f http.HandlerFunc) http.HandlerFunc {
+
+		// Define the http.HandlerFunc
+		return func(w http.ResponseWriter, r *http.Request) {
+
+			// Do middleware things
+			start := time.Now()
+			defer func() { log.Println(r.URL.Path, time.Since(start)) }()
+
+			// Call the next middleware/handler in chain
+			f(w, r)
+		}
+	}
+}
+
+// Method ensures that url can only be requested with a specific method, else returns a 400 Bad Request
+func Method(m string) Middleware {
+
+	// Create a new Middleware
+	return func(f http.HandlerFunc) http.HandlerFunc {
+
+		// Define the http.HandlerFunc
+		return func(w http.ResponseWriter, r *http.Request) {
+
+			// Do middleware things
+			if r.Method != m {
+				http.Error(w, http.StatusText(http.StatusBadRequest), http.StatusBadRequest)
+				return
+			}
+
+			// Call the next middleware/handler in chain
+			f(w, r)
+		}
+	}
+}
+
+// Chain applies middlewares to a http.HandlerFunc
+func Chain(f http.HandlerFunc, middlewares ...Middleware) http.HandlerFunc {
+	for _, m := range middlewares {
+		f = m(f)
+	}
+	return f
+}
+
+func Hello(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "hello world")
+}
+
+// JSON Example
+type User struct {
+	Firstname string `json:"firstname"`
+	Lastname  string `json:"lastname"`
+	Age       int    `json:"age"`
+}
+
+func decodeJson(w http.ResponseWriter, r *http.Request)  {
+	var user User
+	json.NewDecoder(r.Body).Decode(&user)
+
+	fmt.Fprintf(w, "%s %s is %d years old!", user.Firstname, user.Lastname, user.Age)
+}
+
+func encodeJson(w http.ResponseWriter, r *http.Request)  {
+	peter := User{
+		Firstname: "John",
+		Lastname:  "Doe",
+		Age:       25,
+	}
+
+	json.NewEncoder(w).Encode(peter)
+}
+
 func main() {
 	//p1 := &Page{Title: "TestPage", Body: []byte("This is a sample Page.")}
 	//p1.save()
@@ -98,8 +202,18 @@ func main() {
 	//fmt.Println(string(p2.Body))
 
 	//http.HandleFunc("/", handler)
-	http.HandleFunc("/view/", viewHandler)
-	http.HandleFunc("/edit/", editHandler)
-	http.HandleFunc("/save/", saveHandler)
-	log.Fatal(http.ListenAndServe(":8080", nil))
+
+	r := mux.NewRouter()
+	fs := http.FileServer(http.Dir("assets/"))
+
+	r.HandleFunc("/view/{title}", viewHandler).Methods("GET")
+	r.HandleFunc("/edit/{title}", editHandler).Methods("GET")
+	r.HandleFunc("/save/{title}", saveHandler).Methods("POST")
+	r.HandleFunc("/foo", logging(foo))
+	r.HandleFunc("/bar", logging(bar))
+	r.Handle("/static/", http.StripPrefix("/static/", fs))
+	r.HandleFunc("/", Chain(Hello, Method("GET"), Logging()))
+	r.HandleFunc("/decode", decodeJson).Methods("POST")
+	r.HandleFunc("/encode", encodeJson).Methods("GET")
+	log.Fatal(http.ListenAndServe(":8080", r))
 }
